8d59743051f48e406d4e83c8f772748d82b9deec
Pierre Pointereau <pierreyves.pointereau@news.com.au>
2015-09-18 12:51:10 +1000

    Uglifying the bundled JS

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,29 @@
+var path = require('path');
+var webpack = require('webpack');
+var Uglify = webpack.optimize.UglifyJsPlugin;
+
 module.exports = {
 	entry: {
 		javascript: "./app/app.js",
 		html: "./index.html"
 	},
 
+	plugins: [
+		new Uglify({
+			test    : /\.(js|json)($|\?)/,
+			minimize: true,
+			compress: { 'negate_iife': false }
+		})
+	],
+
 	output: {
 		filename: "app.js",
-		path: __dirname + "/dist",
+		path: path.resolve(__dirname, "dist"),
+		publicPath: "/assets/"
 	},
 	module: {
 		loaders: [
-			{ test: /\.js$/, exclude: /node_modules/, loaders: ["react-hot", "babel-loader"] },
+			{ test: /\.js$/, exclude: /node_modules/, loaders: ["babel-loader"] },
 			{ test: /\.json$/, loaders: ["json-loader"] },
 			{ test: /\.html$/, loader: "file?name=[name].[ext]" },
 			{ test: /\.scss$/, loader: "style!css!sass" },
